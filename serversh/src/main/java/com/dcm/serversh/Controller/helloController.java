dc9337c0adc8660e02c94b95e18277b1d329bb95
Dengcm <981652937@qq.com>
2019-04-30 15:06:36 +0800

    Update helloController.java

--- serversh/src/main/java/com/dcm/serversh/Controller/helloController.java
+++ serversh/src/main/java/com/dcm/serversh/Controller/helloController.java
@@ -1,16 +1,34 @@
 package com.dcm.serversh.Controller;
 
+import com.dcm.serversh.Model.User;
+import com.dcm.serversh.Server.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class helloController {
+    //show index.html
     @GetMapping("/index")
     public ModelAndView getIndex()
     {
         ModelAndView mv=new ModelAndView();
         mv.setViewName("index");
         return mv;
+    }
+
+    //for example:insert into database
+    @Autowired
+    private UserService userService;
+    @GetMapping("/add")
+    public String addUser()
+    {
+        User user=new User();
+        user.setId("123456");
+        user.setName("jack");
+        userService.addUser(user);
+
+        return "hello:"+user.getName();
     }
 }
